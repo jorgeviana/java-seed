4064e6a2a9f2c046699a46308127e3c275285d00
Jorge Viana <secoviana@gmail.com>
2019-03-15 15:23:58 +0100

    initial commit

---
+++ src/test/java/io/github/jorgeviana/StartHere.java
@@ -0,0 +1,13 @@
+package io.github.jorgeviana;
+
+import org.junit.Test;
+
+import static org.junit.Assert.fail;
+
+public class StartHere {
+
+    @Test
+    public void failing_test() {
+        fail("fix me!");
+    }
+}
