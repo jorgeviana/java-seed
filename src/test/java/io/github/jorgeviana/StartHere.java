719673fbe83b07b812f80b6b35515d47918bceaa
Jorge Viana <secoviana@gmail.com>
2019-03-26 00:30:12 +0100

    adding example of parameterized test

--- src/test/java/io/github/jorgeviana/StartHere.java
+++ src/test/java/io/github/jorgeviana/StartHere.java
@@ -1,7 +1,10 @@
 package io.github.jorgeviana;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
+import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.fail;
 
 class StartHere {
@@ -9,5 +12,16 @@ class StartHere {
     @Test
     void failing_test() {
         fail("fix me!");
+    }
+
+    @ParameterizedTest(name = "{index}: the max of {0} and {1} is {2}")
+    @CsvSource({
+//           a, b, max
+            "1, 2, 2",
+            "9, 3, 9",
+            "1, 1, 1"
+    })
+    void testing_max(int a, int b, int max) {
+        assertThat(Math.max(a, b)).isEqualTo(max);
     }
 }
